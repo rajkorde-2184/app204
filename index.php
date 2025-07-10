74656b50b9d83182622d9940e0f68b452311987c
rajkorde-2184 <rajkorde68@gmail.com>
2025-07-10 11:21:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <h1>THIS IS A WEB APP</h1>
+    <?php
+      echo "<h1>this app is created using azure app services!!</h1>";
+      ?>
+<a href="about.php">about US</a>
+  </body>
+</html>
